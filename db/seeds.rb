e8bd3ad254cb20ff4409321e165bb8b804b33c27
Alex Kleissner <hex337@gmail.com>
2015-04-04 17:59:53 -0700

    Added all the belts to the Seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+order = 0
+['white', 'high white', 'yellow', 'high yellow', 'green', 'high green', 'blue', 'high blue', 'red', 'high red', 'black'].each do |belt|
+  order = order + 1
+  Belt.create_with(is_active: true, order_by: order).find_or_create_by(name: belt)
+end
