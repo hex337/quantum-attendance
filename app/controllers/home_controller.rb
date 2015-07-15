bef72b3f56c965b5d00c77b992fc0850de222dd3
Alex Kleissner <hex337@gmail.com>
2015-07-14 23:15:12 -0700

    Added the poor attendance query.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  POOR_COUNT = 3
+
   def index
   end
 
@@ -6,5 +8,19 @@ class HomeController < ApplicationController
   end
 
   def poor_attendance
+    meetingIds = Meeting.find_by_sql("
+      SELECT meeting_id as id, count(*)
+      FROM meeting_members
+      JOIN meetings ON meeting_members.meeting_id = meetings.id
+      WHERE meeting_members.meeting_id = meetings.id
+        AND meetings.met > '#{(Time.now - 90.days).to_s(:db)}'
+      GROUP BY meeting_id
+      HAVING count(*) <= #{POOR_COUNT}
+      ORDER BY id DESC
+    ")
+
+    @meetings = meetingIds.collect{|meeting| Meeting.find(meeting.id)}
+
+    render 'meetings/index'
   end
 end
--- app/models/meeting_member.rb
+++ app/models/meeting_member.rb
@@ -3,4 +3,6 @@ class MeetingMember < ActiveRecord::Base
   belongs_to :meeting
   belongs_to :belt
   belongs_to :role
+
+  scope :met_after, ->(time) { where("met > ?", time) }
 end
