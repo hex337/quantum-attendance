4a74dce1cd2a88be203ba08e6f1782bbd065cf39
Alex Kleissner <hex337@gmail.com>
2015-07-17 22:22:34 -0700

    A bunch of updates mostly dealing with the slacker report.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,29 @@ class HomeController < ApplicationController
   end
 
   def slacker_report
+    history_days = 90
+    recent_days = 14
+
+    memberIds = Member.find_by_sql("
+      SELECT DISTINCT m3.id
+      FROM meetings m1
+      INNER JOIN meeting_members m2 ON m1.id = m2.meeting_id
+      INNER JOIN members m3 on m2.member_id = m3.id
+      WHERE m1.met > '#{(Time.now - history_days.days).to_s(:db)}' 
+        AND m3.id NOT IN
+        (
+          SELECT DISTINCT m6.id
+          FROM meetings m4
+          INNER JOIN meeting_members m5 ON m4.id = m5.meeting_id
+          INNER JOIN members m6 ON m5.member_id = m6.id
+          WHERE m1.met > '#{(Time.now - recent_days.days).to_s(:db)}'
+        )
+    ")
+
+    memberIds = memberIds.collect{|mem| mem.id}
+    @members = Member.find(memberIds)
+
+    render 'members/index'
   end
 
   def poor_attendance
@@ -19,7 +42,8 @@ class HomeController < ApplicationController
       ORDER BY id DESC
     ")
 
-    @meetings = meetingIds.collect{|meeting| Meeting.find(meeting.id)}
+    meetingIds = meetingIds.collect{|meeting| meeting.id}
+    @meetings = Meeting.find(meetingIds)
 
     render 'meetings/index'
   end
