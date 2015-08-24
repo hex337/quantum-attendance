c579b644f1a00e3242e4c7a61d447cea36d9bb02
Alex Kleissner <hex337@gmail.com>
2015-08-23 19:57:19 -0700

    Allow slugs as params.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -69,6 +69,6 @@ class SchoolsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def school_params
-      params.require(:school).permit(:name, :comment, :is_active)
+      params.require(:school).permit(:name, :slug, :comment, :is_active)
     end
 end
