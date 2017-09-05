0d71b7b1ebe97aaea69dec6d41d3fcf7ba7dbf7f
Alex Kleissner <hex337@gmail.com>
2017-09-04 23:16:50 -0700

    Adds the belt info to the students list.

---
+++ client/app/bundles/Attendance/components/StudentsScreen/BeltIcon.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BaseComponent from '../BaseComponent';
+
+export default class BeltIcon extends BaseComponent {
+  static propTypes = {
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+  }
+
+  render() {
+    const { name, height, width, id } = this.props;
+    const clsName = "belt-image " + name.replace(/ /g, "-").toLowerCase();
+
+    return (
+      <div key={id} className="belt-tile-container">
+				<svg viewBox="0, 0, 100, 80" width={width} height={height} xmlns="http://www.w3.org/2000/svg" className={clsName}>
+					 <g>
+						<title>Layer 1</title>
+						<polygon points="44.952 22.46 26.647 51.383 36.017 57.306 58.074 22.46" id="svg_2" fill="black" className="belt-main"/>
+						<polygon points="42.39400100708008,22.459999084472656 15.145999908447266,22.459999084472656 15.145999908447266,35.44599914550781 34.172000885009766,35.44599914550781 " id="svg_3" className="belt-main"/>
+						<g id="svg_4">
+						 <path d="m60.625,22.459999l-1.68,2.665001l4.326,10.256001l0.205002,0c0.007,0.021999 21.91,0.065998 21.91,0.065998l0,-12.987l-24.761002,0z" id="svg_5" className="belt-main"/>
+						 <polygon points="50.75,38.69599914550781 58.33000183105469,57.689002990722656 68.49099731445312,53.29399871826172 57.55400085449219,27.394001007080078 " id="svg_6" className="belt-main"/>
+						</g>
+						<rect transform="rotate(32.4771 35.9754 46.9636)" fill="#ffffff" strokeWidth="null" strokeDasharray="null" strokeLinejoin="null" strokeLinecap="null" x="30.520357" y="44.791818" width="10.910116" height="4.343599" id="svg_9" className="belt-stripe" />
+						<rect id="svg_1" transform="rotate(-23.4809 60.625 48.7041)" fill="#ffffff" strokeWidth="null" strokeDasharray="null" strokeLinejoin="null" strokeLinecap="null" x="55.178695" y="46.298061" width="10.892653" height="4.811992" className="belt-stripe" />
+					</g>
+				</svg>
+      </div>
+    );
+  }
+};
--- client/app/bundles/Attendance/components/StudentsScreen/StudentsList/StudentsList.jsx
+++ client/app/bundles/Attendance/components/StudentsScreen/StudentsList/StudentsList.jsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router';
 
 import StudentsListPropTypes from './PropTypes';
 
+import BeltIcon from '../BeltIcon';
+
 export default class StudentsList extends BaseComponent {
   static propTypes = StudentsListPropTypes;
   constructor(props, context) {
@@ -16,25 +18,36 @@ export default class StudentsList extends BaseComponent {
   render() {
     const { $$students } = this.props
     const students = $$students && $$students.get('students') ? $$students.get('students').valueSeq() : [];
+    const belts = $$students && $$students.get('belts') ? $$students.get('belts') : {};
+
     const studentRows = students.map(($$student) =>
       <tr key={$$student.get('id')}>
+        <td>
+          <BeltIcon width={30} height={24} name={belts.get($$student.get("belt").toString()).get("name")} id={$$student.get("belt")} />
+        </td>
         <td>{$$student.get('id')}</td>
         <td><Link to={'/test/students/' + $$student.get('id')}>{$$student.get('first_name')} {$$student.get('last_name')}</Link></td>
       </tr>
     );
 
+    console.log("rows:");
+    console.log(studentRows);
+
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {studentRows}
-        </tbody>
-      </table>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>belt</th>
+              <th>id</th>
+              <th>name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {studentRows}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
