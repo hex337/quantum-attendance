d53ac2f363150beafbac81115d034d044200300d
Alex Kleissner <alex@keen.io>
2017-09-07 12:59:35 -0700

    Fix the test for ClassesList

--- client/app/bundles/Attendance/components/ClassesScreen/ClassesList/ClassesList.test.jsx
+++ client/app/bundles/Attendance/components/ClassesScreen/ClassesList/ClassesList.test.jsx
@@ -17,9 +17,9 @@ describe("ClassesList", () => {
 
   beforeEach(() => {
     props = {
-      $$attendance: fromJS({
-        classes: Immutable.Map()
-      })
+      classes: fromJS(
+        Immutable.Map()
+      )
     };
     mountedClassesList = undefined;
   });
