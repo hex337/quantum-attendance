adf6ccc7f8364f271a1d4102dc87cc93a9ee82d8
Alex Kleissner <alexk@chime.com>
2020-04-09 09:10:22 -0700

    Clean up and still show a class if there is no instructor

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,36 +4,36 @@ class Meeting < ActiveRecord::Base
   scope :met_between, ->(start_time, end_time) { where("met >= ? AND met < ?", end_time, start_time) }
   scope :met_after, ->(time) { where("met > ?", time) }
 
-  scope :for_school, lambda {|school|
+  scope :for_school, lambda { |school|
     where(school_id: school.id) unless school.nil?
   }
 
   belongs_to :meeting_type
   belongs_to :school
   has_many :meeting_members
-  has_many :members, :through => :meeting_members
+  has_many :members, through: :meeting_members
 
   @_instructor = nil
 
   def instructor
-    @_instructor || self._get_instructor
+    @_instructor || _get_instructor
   end
 
   def students
-    @students ||= self.meeting_members.select { |mm| mm.role_id == Role::student_role_id }
+    @students ||= meeting_members.select { |mm| mm.role_id == Role.student_role_id }
   end
 
   def assistants
-    @assistants ||= self.meeting_members.select { |mm| mm.role_id == Role::assistant_role_id }
+    @assistants ||= meeting_members.select { |mm| mm.role_id == Role.assistant_role_id }
   end
   
   def pretty_name
-    "#{self.met.strftime('%a %b %d, %Y')}: #{self.meeting_type.name}"
+    "#{met.strftime('%a %b %d, %Y')}: #{meeting_type.name}"
   end
 
   def _get_instructor
-    self.meeting_members.each do |mm|
-      if mm.role_id == Role::teacher_role_id
+    meeting_members.each do |mm|
+      if mm.role_id == Role.teacher_role_id
         @_instructor = mm.member
         return @_instructor
       end
