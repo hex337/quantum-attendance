d7d15b5ff4c2405b4c333c1242f6af2782d38af4
Alex Kleissner <alexk@chime.com>
2024-11-22 13:59:58 -0800

    Ran the upgrade task in rails
    This needs to get verified still, but should update everything to start
    using rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,19 +10,13 @@ module QuantumAttendance
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
-    config.assets.compile = false
 
-    # This will have the app show times in pacific time zone values
-    config.time_zone = 'Pacific Time (US & Canada)'
-    
-    Rails.application.config.action_view.default_enforce_utf8 = false
-    Rails.application.config.action_dispatch.use_cookies_with_metadata = true
-    Rails.application.config.active_job.return_false_on_aborted_enqueue = true
-    Rails.application.config.active_record.collection_cache_versioning = true
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
