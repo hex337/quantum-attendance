0cd1db62d382aaafd8cc754b831b3c3ae08e3f0e
Alex Kleissner <alexk@chime.com>
2024-12-02 08:22:15 -0800

    Update configs

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module QuantumAttendance
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Pacific Time (US & Canada)"
   end
 end
